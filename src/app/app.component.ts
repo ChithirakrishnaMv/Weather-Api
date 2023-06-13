6bb87451222d76fcb94cb44fbdb496f54b151b3a
ChithirakrishnaMv <chithirakrishnamv@gmail.com>
2023-06-13 21:10:34 +0530

    Weather Api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Weather } from './models/weather';
+import { ApiService } from './services/api.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApi';
+export class AppComponent implements OnInit {
+
+  weat=false
+  weath:string=""
+  img="https://i.pinimg.com/originals/c8/ea/05/c8ea057e5b33769d5027d8f8a2b45a23.gif"
+  weatherdata?:Weather;
+
+  constructor(private api:ApiService){}
+
+  ngOnInit(): void {
+
+  }
+  getweth(){
+    this.api.getWeather(this.weath).subscribe((res:any)=>{
+      this.weat=true
+         this.weatherdata=res
+         if(res.main.temp<=301){
+         this.img="https://cdn.wallpapersafari.com/89/32/qBUXso.gif"
+         }
+         else if(res.main.temp>=301 && res.main.temp>=311){
+          this.img="https://media.tenor.com/aub6VgMv_RoAAAAM/desert-sky.gif"
+         }
+         else {
+          this.img="https://goodmorningwallpapers.info/wp-content/uploads/2023/01/Good-Morning-Gif-Nature.gif"
+         }
+      
+       })
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor( private http: HttpClient) { }
+  getWeather(city:any){
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8b146869647d6adf34618e9a8c3bd805`)
+  }
+ 
+}
+
